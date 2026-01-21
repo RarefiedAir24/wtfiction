e87021f25db811851f99e39312b8f3a55c2f06d6
RarefiedAir24 <supergeek@me.com>
2026-01-20 20:11:17 -0500

    Update episode: Heat Age vs Ice Age: Which Future Are We In?

--- data/scenarios.ts
+++ data/scenarios.ts
@@ -23,42 +23,14 @@ export const scenarios: Scenario[] = [
     publishDate: '2026-01-08',
     featured: true,
   },
-  {
+    {
     id: '8ytNzzit528',
     title: 'Heat Age vs Ice Age: Which Future Are We In?',
-    premise: 'The last Ice Age nearly wiped out humanity.
-
-But what if the next global age isn’t ice?
-
-What if it’s heat?
-
-In this episode of WTFiction, we explore a grounded, science-based what-if scenario:
-
-What't ice? What if it\'s heat?',
+    premise: 'The last Ice Age nearly wiped out humanity.',
     runtime: '04:12',
     youtubeUrl: 'https://youtu.be/8ytNzzit528',
     thumbnailUrl: 'https://i.ytimg.com/vi/8ytNzzit528/maxresdefault.jpg',
     publishDate: '2025-12-18',
-    featured: true,
-  },
-      {
-    id: 'sPklz6qf1h0',
-    title: 'What If AI Was Elected to Run a Country? #viralvideo',
-    premise: 'What if replacing world leaders with AI wasn’t a choice — but the end result of efficiency',
-    runtime: '07:43',
-    youtubeUrl: 'https://youtu.be/sPklz6qf1h0',
-    thumbnailUrl: 'https://i.ytimg.com/vi/sPklz6qf1h0/maxresdefault.jpg',
-    publishDate: '2025-12-30',
-    featured: true,
-  },
-  {
-    id: 'what-if-venezuela-attacked-u-s-oil-platforms-video',
-    title: 'What If Venezuela Attacked U.S. Oil Platforms? #video',
-    premise: 'America’s offshore oil infrastructure powers millions of homes, fuels global markets, and underpins national energy security's offshore oil infrastructure powers millions of homes, fuels global markets, and underpins national energy security',
-    runtime: '07:21',
-    youtubeUrl: 'https://youtu.be/i8vIIkaU4wI',
-    thumbnailUrl: 'https://i.ytimg.com/vi/i8vIIkaU4wI/maxresdefault.jpg',
-    publishDate: '2025-12-17',
     featured: true,
   },
 ];
