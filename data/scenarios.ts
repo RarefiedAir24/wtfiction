08574a765962cec68e8e874616ff7a9355d350af
RarefiedAir24 <supergeek@me.com>
2026-01-21 07:59:45 -0500

    Delete episode: qBl6FCVTA8E

--- data/scenarios.ts
+++ data/scenarios.ts
@@ -52,18 +52,7 @@ export const scenarios: Scenario[] = [
     thumbnailUrl: 'https://i.ytimg.com/vi/i8vIIkaU4wI/maxresdefault.jpg',
     publishDate: '2025-12-17',
     featured: true,
-  },
-  {
-    id: 'qBl6FCVTA8E',
-    title: 'What If Survival Wasn’t the Reason We Work?'t the Reason We Work?',
-    premise: 'For most of human history, survival has been the reason we work',
-    runtime: '05:17',
-    youtubeUrl: 'https://youtu.be/qBl6FCVTA8E',
-    thumbnailUrl: 'https://i.ytimg.com/vi/qBl6FCVTA8E/hqdefault.jpg',
-    publishDate: '2026-01-18',
-    featured: true,
-  },
-];
+  }];
 
 export const featuredScenarios = scenarios.filter(s => s.featured);
 
