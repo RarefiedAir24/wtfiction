9d46749908a17dfac7994e9ee7fa66133f2f8d9e
Frank Schioppa <supergeek@me.com>
2026-01-21 07:24:51 -0500

    Fix ALL syntax errors: comprehensive fix for all duplicate text

--- data/scenarios.ts
+++ data/scenarios.ts
@@ -34,7 +34,7 @@ What if it’s heat?
 
 In this episode of WTFiction, we explore a grounded, science-based what-if scenario:
 
-What't ice? What if it\'s heat? In this episode of WTFiction, we explore a grounded, science-based what-if scenario.',
+What'The last Ice Age nearly wiped out humanity. But what if the next global age isn\'t ice? What if it\'s heat? In this episode of WTFiction, we explore a grounded, science-based what-if scenario.',
     runtime: '04:12',
     youtubeUrl: 'https://youtu.be/8ytNzzit528',
     thumbnailUrl: 'https://i.ytimg.com/vi/8ytNzzit528/maxresdefault.jpg',
