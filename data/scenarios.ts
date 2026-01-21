4cfaa67bde01784646a1af577330a3538ba8627a
Frank Schioppa <supergeek@me.com>
2026-01-20 22:02:06 -0500

    Fix remaining syntax errors in scenarios.ts

--- data/scenarios.ts
+++ data/scenarios.ts
@@ -34,7 +34,7 @@ What if it’s heat?
 
 In this episode of WTFiction, we explore a grounded, science-based what-if scenario:
 
-What't ice? What if it\'s heat?',
+What's heat?',
     runtime: '04:12',
     youtubeUrl: 'https://youtu.be/8ytNzzit528',
     thumbnailUrl: 'https://i.ytimg.com/vi/8ytNzzit528/maxresdefault.jpg',
